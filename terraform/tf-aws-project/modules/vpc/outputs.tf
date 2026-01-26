ee225e68d9287ec0c792f8e4473804702f8626fc
Ubuntu <ubuntu@ip-172-31-37-29.ap-south-1.compute.internal>
2026-01-26 19:13:03 +0000

    created vpc via terraform

---
+++ terraform/tf-aws-project/modules/vpc/outputs.tf
@@ -0,0 +1,4 @@
+output "vpc" {
+  description = "Id of VPC"
+  value       = aws_vpc.tf-project-vpc.id
+}
---
+++ terraform/tf-aws-project/modules/vpc/variables.tf
@@ -0,0 +1,6 @@
+
+variable "vpc-cidr" {
+  description = "vpc cidr value"
+  type        = string
+  default     = "10.0.0.0/16"
+}
