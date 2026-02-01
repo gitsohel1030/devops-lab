4b512c5c13e48afb6b875ad7c2fb2e045594c706
Ubuntu <ubuntu@ip-172-31-37-29.ap-south-1.compute.internal>
2026-02-01 14:06:32 +0000

    added dynamoDB locking for state file

--- terraform/tf-aws-project/envs/prod/backend.tf
+++ terraform/tf-aws-project/envs/prod/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "tf-project-dev-state"
     key     = "prod/terraform.tfstate"
+    dynamodb_table = "tf_state_locks"
     region  = "ap-south-1"
     encrypt = true
   }
---
+++ terraform/tf-aws-project/envs/prod/dynamodb.tf
@@ -0,0 +1,15 @@
+resource "aws_dynamodb_table" "tf_state_locks" {
+  name = "tf_state_locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  
+  tags = {
+    name = "tf-state-lock"
+  }
+  
+}
