a88fdaf18520519f396f4e61f7e76f734dc0b502
Ubuntu <ubuntu@ip-172-31-37-29.ap-south-1.compute.internal>
2026-02-03 13:38:28 +0000

    commented dynamodb module

--- terraform/tf-aws-project/envs/prod/dynamodb.tf
+++ terraform/tf-aws-project/envs/prod/dynamodb.tf
@@ -1,15 +1,15 @@
-resource "aws_dynamodb_table" "tf_state_locks" {
-  name = "tf_state_locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
+# resource "aws_dynamodb_table" "tf_state_locks" {
+#   name = "tf_state_locks"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
   
-  tags = {
-    name = "tf-state-lock"
-  }
+#   tags = {
+#     name = "tf-state-lock"
+#   }
   
-}
+# }
