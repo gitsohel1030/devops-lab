ba8b5bbcc315090495a13bd4198eeeaf66b6b0b2
Ubuntu <ubuntu@ip-172-31-37-29.ap-south-1.compute.internal>
2026-02-03 06:31:01 +0000

    testing tf jenkins cicd

--- terraform/SecGrp.tf
+++ terraform/SecGrp.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "TF_demo_SG" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_sshfromMyIP" {
   security_group_id = aws_security_group.TF_demo_SG.id
-  cidr_ipv4         = "167.103.22.197/32"
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
